e9f873ea40d009958b03b39e0e0266d0ccb9d88a
jazilzaim <jazilzaim32@gmail.com>
2022-01-03 14:46:52 -0800

    Working on stuff

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import styled from 'styled-components';
 
 export default function App() {
   return (
