1d765af08f3e5e31aa65d55f84390c44008b3c38
jazilzaim <jazilzaim32@gmail.com>
2022-01-06 09:54:18 -0800

    magic works

---
+++ utils/magicauth.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Magic } from '@magic-sdk/react-native';
+import { 
+    StyleSheet,
+    Text, 
+    View,
+    Button,
+    SafeAreaView,
+    Platform,
+} from 'react-native';
+
+
+
+// Magic API key
+const m = new Magic('pk_live_55D93A0BD91B3D6E'); // ✨
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
