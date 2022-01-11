e8797bb15df9ed4b76fa78226c336b5031855c43
jazilzaim <jazilzaim32@gmail.com>
2022-01-11 12:54:13 -0800

    in the works'

---
+++ src/navigator/Profile /profile.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { 
+    View
+} from 'react-native';
+
+// Creating user profile 
+// Querying from GraphQL
+
+class Profile extends React.Component { 
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
